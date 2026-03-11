87c536eecf1f8739447e6b6ff2903ddaff09c6ab
thiago sousa <thiagosohsa@gmail.com>
2026-03-11 09:30:33 -0300

    refactor: add bidirectional relationship Category and Product

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,7 +3,9 @@ package com.devsuperior.dscatalog.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -16,6 +18,9 @@ public class Category {
     private Instant createdAt;
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updateAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -47,6 +52,10 @@ public class Category {
 
     public Instant getUpdateAt() {
         return updateAt;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @PrePersist
